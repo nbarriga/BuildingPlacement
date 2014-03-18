5ad038cdb0b7abc6c5466ab37048d6968a79eaf0
Nicolas A. Barriga <nbarriga@gmail.com>
2014-03-18 15:16:07 -0600

    Added new config option BaseAssaultStateDescriptionFile.

--- src/BuildingPlacementExperiment.cpp
+++ src/BuildingPlacementExperiment.cpp
@@ -62,10 +62,14 @@ void BuildingPlacementExperiment::runExperiment(){
 	gaDefDivFlag=gaTrue;
 	GARandomSeed(time(NULL));
 
-	// set the map file for all states
-	for (size_t state(0); state < states.size(); ++state)
-	{
-		states[state].setMap(*map);
+//	// set the map file for all states
+//	for (size_t state(0); state < states.size(); ++state)
+//	{
+//		states[state].setMap(*map);
+//	}
+
+	if(!map){
+	    map=new Map(64,64);
 	}
 
 #ifdef USING_VISUALIZATION_LIBRARIES
@@ -85,17 +89,17 @@ void BuildingPlacementExperiment::runExperiment(){
 		for (size_t p2Player(0); p2Player < players[1].size(); p2Player++)
 		{
 			// for each state we care about
-			for (size_t state(0); state < states.size(); ++state)
+			for (size_t state(0); state < _fixedBuildings.size(); ++state)
 			{
 //				char buf[255];
-				fprintf(stderr, "%s  ", configFileSmall.c_str());
-				fprintf(stderr, "%5d %5d %5d %5d", (int)p1Player, (int)p2Player, (int)state, (int)states[state].numUnits(Players::Player_One));
-
-
-				resultsPlayers[0].push_back(p1Player);
-				resultsPlayers[1].push_back(p2Player);
-				resultsStateNumber[p1Player][p2Player].push_back(state);
-				resultsNumUnits[p1Player][p2Player].push_back(states[state].numUnits(Players::Player_One));
+//				fprintf(stderr, "%s  ", configFileSmall.c_str());
+//				fprintf(stderr, "%5d %5d %5d %5d", (int)p1Player, (int)p2Player, (int)state, (int)states[state].numUnits(Players::Player_One));
+//
+//
+//				resultsPlayers[0].push_back(p1Player);
+//				resultsPlayers[1].push_back(p2Player);
+//				resultsStateNumber[p1Player][p2Player].push_back(state);
+//				resultsNumUnits[p1Player][p2Player].push_back(states[state].numUnits(Players::Player_One));
 
 				// get player one
 				PlayerPtr playerOne(players[0][p1Player]);
@@ -119,43 +123,45 @@ void BuildingPlacementExperiment::runExperiment(){
 				// we want to use in the GA.  The ga doesn't operate on this genome in the
 				// optimization - it just uses it to clone a population of genomes.
 
-				std::vector<Unit> fixedBuildings,buildings,attackers,defenders;
-				int units=states[state].numUnits(Players::Player_One)+states[state].numUnits(Players::Player_Two);
-				for(int i=0;i<units;i++){
-					const Unit &unit=states[state].getUnitByID(i);
-					if(unit.player()==Players::Player_One){//attacker
-						attackers.push_back(unit);
-					}else if(unit.player()==Players::Player_Two){//defender
-						if(unit.type().isBuilding()){
-							buildings.push_back(unit);
-						}else{
-							defenders.push_back(unit);
-						}
-					}else{
-						System::FatalError("More than 2 players?");
-					}
-				}
-				const SparCraft::Unit nexus(BWAPI::UnitTypes::Protoss_Nexus,
-						Players::Player_Two,
-						SparCraft::Position(TILE_SIZE*2+BWAPI::UnitTypes::Protoss_Nexus.tileWidth()/2.0f*TILE_SIZE,
-								TILE_SIZE*55+BWAPI::UnitTypes::Protoss_Nexus.tileHeight()/2.0f*TILE_SIZE));
-				fixedBuildings.push_back(nexus);
+//				std::vector<Unit> fixedBuildings,buildings,attackers,defenders;
+//				int units=states[state].numUnits(Players::Player_One)+states[state].numUnits(Players::Player_Two);
+//				for(int i=0;i<units;i++){
+//					const Unit &unit=states[state].getUnitByID(i);
+//					if(unit.player()==Players::Player_One){//attacker
+//						attackers.push_back(unit);
+//					}else if(unit.player()==Players::Player_Two){//defender
+//						if(unit.type().isBuilding()){
+//							buildings.push_back(unit);
+//						}else{
+//							defenders.push_back(unit);
+//						}
+//					}else{
+//						System::FatalError("More than 2 players?");
+//					}
+//				}
+//				const SparCraft::Unit nexus(BWAPI::UnitTypes::Protoss_Nexus,
+//						Players::Player_Two,
+//						SparCraft::Position(TILE_SIZE*2+BWAPI::UnitTypes::Protoss_Nexus.tileWidth()/2.0f*TILE_SIZE,
+//								TILE_SIZE*55+BWAPI::UnitTypes::Protoss_Nexus.tileHeight()/2.0f*TILE_SIZE));
+//				fixedBuildings.push_back(nexus);
 
-				std::cout<<"fixed buildings: "<<fixedBuildings.size()<<
-						", buildings: "<<buildings.size()<<
-						", defenders: "<<defenders.size()<<
-						",attackers: "<<attackers.size()<<std::endl;
+				std::cout<<"fixed buildings: "<<_fixedBuildings[state].size()<<
+						", buildings: "<<_buildings[state].size()<<
+						", defenders: "<<_defenders[state].size()<<
+						",attackers: "<<_attackers[state].size()<<std::endl;
 
 
 
-				GeneticOperators::configure(fixedBuildings,
-						buildings,
-						defenders,
-						attackers,
+				svv expDesc;
+				expDesc.push_back(sv());
+				GeneticOperators::configure(_fixedBuildings[state],
+				        _buildings[state],
+				        _defenders[state],
+				        _attackers[state],
 						map,_display,
 						playerOne,
 						playerTwo,
-						getExpDescription(0,0,0));
+						expDesc);
 
 				GAListGenome<Gene> genome(GeneticOperators::Objective);
 
@@ -196,5 +202,104 @@ void BuildingPlacementExperiment::runExperiment(){
 
 
 }
+
+void BuildingPlacementExperiment::addState(const std::string& line) {
+
+    std::istringstream iss(line);
+
+    // the first number is the playerID
+    std::string state;
+    std::string stateType;
+    int numStates;
+
+    iss >> state;
+    iss >> stateType;
+    iss >> numStates;
+    if (strcmp(stateType.c_str(), "BaseAssaultStateDescriptionFile") == 0)
+    {
+        std::string filename;
+        iss >> filename;
+
+        for (int i(0); i<numStates; ++i)
+        {
+            parseBaseAssaultStateDescriptionFile(filename);
+        }
+    }
+    else{
+        SparCraft::System::FatalError("State type not supported");
+        SearchExperiment::addState(line);
+    }
 }
 
+
+void BuildingPlacementExperiment::parseBaseAssaultStateDescriptionFile(
+        const std::string& fileName) {
+    std::vector<std::string> lines = getLines(fileName);
+
+     std::vector<Unit> fixedBuildings,buildings,attackers,defenders;
+
+     for (size_t u(0); u<lines.size(); ++u)
+     {
+         std::stringstream iss(lines[u]);
+         std::string unitType;
+         int playerID,x,y,hp,time,hpFinal;
+         bool fixed;
+
+         iss >> unitType;
+         iss >> playerID;
+         iss >> x;
+         iss >> y;
+         iss >> hp;
+         iss >> time;
+         iss >> fixed;
+         iss >> hpFinal;
+
+         BWAPI::UnitType type(getUnitType(unitType));
+         if(!type.isBuilding()&&fixed){
+             SparCraft::System::FatalError("A non building unit cannot be fixed");
+         }
+
+         if(type==BWAPI::UnitTypes::Terran_Refinery){
+             SparCraft::System::FatalError("Refinery not supported");
+         }
+
+         if(type.isBuilding()){
+             int xoffset=(x-(type.tileWidth()*TILE_SIZE/2))%TILE_SIZE;
+             int yoffset=(y-(type.tileHeight()*TILE_SIZE/2))%TILE_SIZE;
+             if(xoffset!=0 || yoffset!=0){
+                 std::cerr<<"Building '"<<unitType<<"' in wrong position "<<Position(x, y).getString()<<
+                         ", closest legal is "<<Position(x-xoffset, y-yoffset).getString()<<
+                         ". Please fix your configuration file."<<std::endl;
+             }
+         }
+
+         Unit unit(type, Position(x, y), 0, playerID, hp,
+                 type == BWAPI::UnitTypes::Terran_Medic ? Constants::Starting_Energy : 0, 0,0);
+
+
+         if(playerID==0){//assault
+             if(type.isBuilding()){
+                 SparCraft::System::FatalError("Assault player cannot have buildings");
+             }else{
+                 attackers.push_back(unit);
+             }
+         }else{//defend
+             if(type.isBuilding()){
+                 if(fixed){
+                     fixedBuildings.push_back(unit);
+                 }else{
+                     buildings.push_back(unit);
+                 }
+             }else{
+                 defenders.push_back(unit);
+             }
+         }
+     }
+
+     _fixedBuildings.push_back(fixedBuildings);
+     _buildings.push_back(buildings);
+     _attackers.push_back(attackers);
+     _defenders.push_back(defenders);
+}
+
+}
--- src/BuildingPlacementExperiment.h
+++ src/BuildingPlacementExperiment.h
@@ -6,10 +6,13 @@
 namespace BuildingPlacement {
 class BuildingPlacementExperiment:public SearchExperiment {
 	Display *_display;
+	std::vector<std::vector<Unit> > _fixedBuildings,_buildings,_attackers,_defenders;
+	void parseBaseAssaultStateDescriptionFile(const std::string & fileName);
 public:
 	BuildingPlacementExperiment(const std::string & configFile);
 	virtual ~BuildingPlacementExperiment();
 	virtual void addPlayer(const std::string & line);//override
+	virtual void addState(const std::string & line);//override
 	void runExperiment();
 };
 
